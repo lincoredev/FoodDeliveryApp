f41a395aea3c215c0ed6883685b83567e3a067e9
Vova Robot <lincore14@mail.ru>
2024-03-24 21:25:21 +0300

    create base project, remove storyboards refs

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Vova Lincore on 24.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
